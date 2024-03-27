7d12dd0e9a4b9ad3e34ace320efa3aaac72b1594
Сивушкова Ольга Максимовна (psheom) <psheom@maxi-net.ru>
2024-03-27 14:42:49 +0300

    Переход на Jupiter
    * Добавили задачу `GitHub`-а на проверку целостности ссылок
    * Определили *Факторизацию* в отдельный проект
    * Начали проект по *Теории автоматов*
    * Описание установки среды в главном `README`
    * Скрипт установки необходимых пакетов `Python`-а

---
+++ install.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Выполняйте только при активной виртуальной среде
+python -m pip install --upgrade pip
+
+pip install notebook
+pip install jupyter
+pip install matplotlib
+pip install transitions
+pip install graphviz pygraphviz
