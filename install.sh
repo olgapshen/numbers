7da046aa6b4bc878ddf7bea70110977cbe29eee0
Сивушкова Ольга Максимовна (psheom) <psheom@maxi-net.ru>
2024-04-01 18:41:50 +0300

    Подключаем pyeda

--- install.sh
+++ install.sh
@@ -6,6 +6,7 @@ python -m pip install --upgrade pip
 pip install notebook
 pip install jupyter
 pip install pandas
+pip install pyeda
 pip install iplantuml
 pip install matplotlib
 pip install transitions
